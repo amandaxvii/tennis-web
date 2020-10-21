2717eabf3ff463ce9b7d1e4eb3dfa1bc39fb2545
Amanda Huang <amanda110xvii@gmail.com>
2020-10-21 18:01:10 +0800

    update section_3-1 page

--- assets/js/section_3-1.js
+++ assets/js/section_3-1.js
@@ -3,13 +3,7 @@ const courtAreaSelect = document.querySelector('#court-area')
 const courtLineSelect = document.querySelector('#court-line')
 const glossarySelect = document.querySelector('.glossary-select')
 const glossaryButton = document.querySelector('.glossary-btn')
-const glossaryButtonTwo = document.querySelector('.glossary-btn-two')
-
-// if (glossaryButton.classList.contains('glossary-btn')) {
-//     console.log('yes')
-// }
-
-
+const glossaryButtonLine = document.querySelector('.glossary-line-btn')
 
 function courtAreaSelected() {
     // selectedIndex 為你所選中的選項index
@@ -25,32 +19,36 @@ function courtAreaSelected() {
     // 將 DOM 裡相對應的 option value class 取出
     const courtTransitions = document.querySelectorAll(`.${optionValue}`)
 
-    // 將上一個 transition 移除
-
-    // for (let i = 0; i < courtTransitions.length; i++) {
-    //     courtTransitions[i].classList.remove('active')
-    //     console.log(courtTransitions[i])
-    // }
-
-
-    glossaryButton.addEventListener('click', function () {
+    // select 下的 option change ，將上一個 transition 移除
+    courtAreaSelect.addEventListener('change', function () {
         courtTransitions.forEach(function (courtTransition) {
             courtTransition.classList.remove('active')
         })
     })
 
+    // 加上判斷式，在同樣的 option 下，再點一下 button，會有 toggle 效果
+    for (let i = 0; i < courtTransitions.length; i++) {
+        console.log(courtTransitions[i])
+        if (courtTransitions[i].classList.contains('active')) {
+            courtTransitions[i].classList.remove('active')
+            console.log('active to non-active')
+        } else {
+            courtTransitions[i].classList.add('active')
+            console.log('non-active to active')
+        }
+    }
+
+    // forEach 寫法
+    //   courtTransitions.forEach(function (courtTransition) {
+    //       courtTransition.classList.add('active')
+    //       console.log(courtTransition)
+    //   })
+
     // 要用迴圈把 quertySelectorAll 的每個 item 取出
     // for (let i = 0; i < courtTransitions.length; i++) {
     //     courtTransitions[i].classList.add('active')
     // }
-
-    // forEach 寫法
-    courtTransitions.forEach(function (courtTransition) {
-        courtTransition.classList.add('active')
-        console.log(courtTransition)
-    })
 }
-
 
 function courtLineSelected() {
     // selectedIndex 為你所選中的選項index
@@ -66,31 +64,43 @@ function courtLineSelected() {
     // 將 DOM 裡相對應的 option value class 取出
     const courtTransitions = document.querySelectorAll(`.${optionValue}`)
 
-    // 將上一個 transition 移除
-
-    // for (let i = 0; i < courtTransitions.length; i++) {
-    //     courtTransitions[i].classList.remove('active')
-    //     console.log(courtTransitions[i])
-    // }
-
-
-    glossaryButton.addEventListener('click', function () {
+    // select 下的 option change ，將上一個 transition 移除
+    courtLineSelect.addEventListener('change', function () {
         courtTransitions.forEach(function (courtTransition) {
-            courtTransition.classList.remove('active')
+            courtTransition.classList.remove('line-active')
         })
     })
 
-    // 要用迴圈把 quertySelectorAll 的每個 item 取出
-    // for (let i = 0; i < courtTransitions.length; i++) {
-    //     courtTransitions[i].classList.add('active')
-    // }
+    // 加上判斷式，在同樣的 option 下，再點一下 button，會有 toggle 效果
+    for (let i = 0; i < courtTransitions.length; i++) {
+        console.log(courtTransitions[i])
+        if (courtTransitions[i].classList.contains('line-active')) {
+            courtTransitions[i].classList.remove('line-active')
 
-    // forEach 寫法
-    courtTransitions.forEach(function (courtTransition) {
-        courtTransition.classList.add('active')
-        console.log(courtTransition)
+            console.log('active to non-active')
+        } else {
+            courtTransitions[i].classList.add('line-active')
+            console.log('non-active to active')
+        }
+    }
+}
+
+function definitionDisplay() {
+    const courtDefinition = document.querySelector('#court-area-definition')
+    console.log(courtDefinition)
+
+    // select 下的 option change ，將上一個 transition 移除
+    courtAreaSelect.addEventListener('change', function () {
+        courtDefinition.style.display = 'none'
     })
+
+    if (courtDefinition.style.display === 'none') {
+        courtDefinition.style.display = 'block'
+    } else {
+        courtDefinition.style.display = 'none'
+    }
 }
 
 glossaryButton.addEventListener('click', courtAreaSelected)
-glossaryButtonTwo.addEventListener('click', courtLineSelected)
+glossaryButton.addEventListener('click', definitionDisplay)
+glossaryButtonLine.addEventListener('click', courtLineSelected)
